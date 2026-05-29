549774644b4a24b12cfe7a049d612b4ba9199cb2
tyakorigy03 <150850269+tyakorigy03@users.noreply.github.com>
2026-05-29 09:22:31 +0200

    Comment out base URL in Vite configuration
    Comment out the base URL configuration in vite.config.js

--- shule_canteen_seller_app/vite.config.js
+++ shule_canteen_seller_app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/cantine/',
+  // base: '/cantine/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
